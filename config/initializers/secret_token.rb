47becdb362bf5ee8c299c3fdaf3b0ca7a205ae69
Stefan Kendall <stefankendall@gmail.com>
2013-01-20 16:41:33 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CraigslistScanner::Application.config.secret_token = '87de5b18056322675528a86021320bcf27de6a43b59bda150ef517c5b3145c9cf3f7c10a00882ce589afc51ea02548880eba590b8973114652b11f8c1d881efa'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+CraigslistScanner::Application.config.session_store :cookie_store, key: '_craigslist-scanner_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# CraigslistScanner::Application.config.session_store :active_record_store
